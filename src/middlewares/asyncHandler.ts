beef3b0ed14f334c45418633051e7e13039940ba
HamzaDev <hmzt25235@gmail.com>
2026-04-30 01:05:21 +0300

    Async Handler

---
+++ src/middlewares/asyncHandler.ts
@@ -0,0 +1,8 @@
+import type { Response, Request, NextFunction, RequestHandler  } from 'express';
+
+// Instead of repeating try and catch
+export const asyncHandler = (fn: RequestHandler): RequestHandler => { // How to use?
+  return (req:Request, res:Response, next:NextFunction) => {
+    Promise.resolve(fn(req, res, next)).catch(next);
+  };
+};
