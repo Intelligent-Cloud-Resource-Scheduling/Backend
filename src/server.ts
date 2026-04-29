aab29d350a3dccb5d12720e3290250c56ff34196
HamzaDev <hmzt25235@gmail.com>
2026-04-30 02:01:54 +0300

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,20 @@
 import express, {type Request, type Response} from "express";
+import userExampleRoutes from '@/routes/user-Example-route.js';
+import { errorHandler } from '@/middlewares/error.js';
+import { requestId } from '@/middlewares/requestId.js';
 
 const App = express();
+
+
+App.use(express.json());
+App.use(requestId);
+
+App.use('/users', userExampleRoutes);
 
 App.get("/", (req: Request, res: Response)=>{
     res.send(`Server is running, main route`)
 })
+
+App.use(errorHandler);
 
 export default App;
