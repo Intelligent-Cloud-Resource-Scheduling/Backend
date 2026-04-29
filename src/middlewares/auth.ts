8cc7b1315b20ed18053de3d75712c969723cadbe
HamzaDev <hmzt25235@gmail.com>
2026-04-30 01:15:22 +0300

    Auth verify middleware

---
+++ src/middlewares/auth.ts
@@ -0,0 +1,15 @@
+import { AppError } from '../utils/AppError.js';
+import type { Response, Request, NextFunction } from 'express';
+
+export const auth = (req:Request, res:Response, next:NextFunction) => { // How to use?
+  const token = req.headers.authorization;
+
+  if (!token) {
+    return next(new AppError('Unauthorized', 401, 'UNAUTHORIZED'));
+  }
+
+  // verify token here
+  
+
+  next();
+};
