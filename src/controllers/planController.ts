e314bffff0f52fe16c0dfe25d9dca325789a62ec
HamzaDev <hmzt25235@gmail.com>
2026-05-03 01:04:27 +0300

    update plan info

--- src/controllers/planController.ts
+++ src/controllers/planController.ts
@@ -18,7 +18,10 @@ export const createPlan = async(req: Request, res:Response) => {
     });
 
     if(existingPlan) {
-        throw new AppError("Plan name already exists, If you are trying ti update, use the update endpoint.", 409, ERRORS.E409);
+        throw new AppError(
+            "Plan name already exists, If you are trying ti update, use the update endpoint.", 
+            409, ERRORS.E409PLAN
+        );
     }
 
     const plan = await prisma.plans.create({
@@ -35,4 +38,54 @@ export const createPlan = async(req: Request, res:Response) => {
     }
 
     return sendSuccess(res, plan, "Plan created successfully", 201);
+}
+
+export const updatePlan = async(req: Request, res:Response) => {
+    const { uuid } = req.params;
+    const { name, description, price, max_uploads_per_week } = req.body;
+
+    const updateData = Object.fromEntries(
+        Object.entries({name, description, price, max_uploads_per_week})
+        .filter(([indx, value]) => value != undefined)
+    )
+
+    if (typeof uuid !== 'string') {
+        throw new AppError("Invalid UUID format", 400, ERRORS.E400);
+    }
+
+    const existingPlan = await prisma.plans.findUnique({
+        where: {uuid},
+    })
+
+    if(!existingPlan){
+        throw new AppError("Plan not found", 404, ERRORS.E404);
+    }
+
+    const dublicatesCheck = [];
+    if(name !== undefined)
+        dublicatesCheck.push({ name })
+    if(price !== 0)
+        dublicatesCheck.push({ price })
+
+    if(dublicatesCheck.length > 0){
+        const duplicates = await prisma.plans.findFirst({
+            where: {
+                AND: [
+                    {OR: dublicatesCheck},
+                    {NOT: {uuid}}
+                ]
+            }
+        })
+
+        if(duplicates) {
+            throw new AppError("Plan name or price already exists", 409, ERRORS.E409PLAN);
+        }
+    }
+
+    const updatedPlan = await prisma.plans.update({
+        where: {uuid},
+        data: updateData
+    })
+
+    return sendSuccess(res, updatedPlan, "Plan updates successfully", 200);
 }
--- src/routes/planRoutes.ts
+++ src/routes/planRoutes.ts
@@ -1,4 +1,4 @@
-import { createPlan } from "@/controllers/planController.js";
+import { createPlan, updatePlan } from "@/controllers/planController.js";
 import { asyncHandler } from "@/middlewares/asyncHandler.js";
 import { validate } from "@/middlewares/validate.js";
 import { Router } from "express";
@@ -13,9 +13,13 @@ const createPlanSchema = z.object({
     max_uploads_per_week: z.number().min(1),
 })
 
-const hideShowPlanSchema = z.boolean();
+const updatePlanSchema = createPlanSchema.partial().refine(
+    (data) => Object.keys(data).length > 0,
+    { message: "At least one field must be provided for update." }
+);
 
-router.post('/create', validate(createPlanSchema), asyncHandler(createPlan))
+router.post('/create', validate(createPlanSchema), asyncHandler(createPlan));
+router.patch('/edit/:uuid', validate(updatePlanSchema), asyncHandler(updatePlan));
 
 
 export default router;
